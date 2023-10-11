562ee7b5cfaa6f10db7f5b3485c5495b5d5354d4
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2023-10-11 21:26:49 +0530

    updated the ui

---
+++ static/js/script.js
@@ -0,0 +1,33 @@
+document.querySelector('#sidebar-toggle-btn-1').addEventListener('click', function(event) {
+    event.stopPropagation(); // Prevent event propagation
+    toggleSidebar();
+});
+
+document.querySelector('#sidebar-toggle-btn-2').addEventListener('click', function(event) {
+    event.stopPropagation(); // Prevent event propagation
+    toggleSidebar();
+});
+
+// Function to toggle the sidebar
+function toggleSidebar() {
+    var sidenav = document.querySelector('.sidenav');
+    sidenav.classList.toggle('open');
+
+    // Check if the sidebar is open
+    if (sidenav.classList.contains('open')) {
+        // Add a click event listener to the entire document to close the sidebar if clicking outside
+        document.addEventListener('click', closeSidebarOnClickOutside);
+    } else {
+        // Remove the click event listener when the sidebar is closed
+        document.removeEventListener('click', closeSidebarOnClickOutside);
+    }
+}
+
+// Function to close the sidebar when clicking outside
+function closeSidebarOnClickOutside(event) {
+    var sidenav = document.querySelector('.sidenav');
+    if (!sidenav.contains(event.target)) {
+        sidenav.classList.remove('open');
+        document.removeEventListener('click', closeSidebarOnClickOutside);
+    }
+}
