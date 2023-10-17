d3a044e84e6d93be86317376670d76afdeedb999
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2023-10-17 06:06:24 +0530

    Deploy1

--- trading/settings.py
+++ trading/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3oi8yggzi$9h8qjhn9c^j7j*8(n+9va&j$d$d)!+w61hrd3gx5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
